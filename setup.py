5cafb0f8cba998d73176aa170d6a3fdadf9e45ca
Matt McCredie <mmccredie@ssni.com>
2017-01-15 20:22:03 -0800

    Adding setup.py so that this module can be installed.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="frozendict",
+      version="0.0",
+      description="Immutable dictionary for python",
+      author="Matt McCredie",
+      author_email="mccredie@gmail.com",
+      url="https://github.com/mccredie/frozendict",
+      py_modules=["frozendict"]
+     )
